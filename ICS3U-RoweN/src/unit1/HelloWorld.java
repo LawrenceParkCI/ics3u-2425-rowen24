4cb6bc43d70ba6df949dc9f808149bbd5d0aca72
343119186 <343119186@C-MXL2393F1F.tdsb.on.ca>
2024-09-20 11:11:13 -0400

    Adding ICS3U project with HelloWorld class

---
+++ ICS3U-RoweN/src/unit1/HelloWorld.java
@@ -0,0 +1,21 @@
+package unit1;
+
+ /**
+ * Description: This program prints Hello World, and
+ * a second statement. <br>
+ * Date: Sept. 19, 2024
+ *@author Noah Rowe
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// This prints hello to the system console
+		System.out.println("Hello World");
+		System.out.println("Burger Maker");
+	
+		/*
+		 *This is ablock comment.
+		 *you can add notes on multiple lines.
+		 */
+	}
+}
