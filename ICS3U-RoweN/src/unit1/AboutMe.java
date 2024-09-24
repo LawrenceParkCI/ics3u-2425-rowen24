7dc52b2632d0595ef957fbed479c8f98131d5af4
343119186 <343119186@C-MXL2393F1F.tdsb.on.ca>
2024-09-24 11:41:01 -0400

    This is some information about me

---
+++ ICS3U-RoweN/src/unit1/AboutMe.java
@@ -0,0 +1,32 @@
+package unit1;
+/**
+ * Description: This program prints Hello World, and
+ * a second statement.
+ * Date: Sept 19, 2024
+ * @author Noah Rowe
+ */
+public class AboutMe{
+	public static void main(String[] args) {
+		// This prints hello to the system console
+		System.out.print("my name is Noah");
+		System.out.println("I go to Lawrence Park");
+		System.out.println("My teacher is Ms.Kemp");
+		System.out.println("In grade 11");
+		System.out.println("Go Panthers!");
+		System.out.println("--------------------------------------------");
+		System.out.format("\n");
+		System.out.println("______________________________________________");
+		System.out.format(" Date: \t \t   Time: \t \t   Period 1 \t \t   period 2 \t \t   period 3 \t \t   Period 4 \t \t");
+		System.out.format("Date: %15sTime: %8s", "Monday", "9:00-10:20");
+		System.out.format("Date: %15sTime: %8s", "Monday", "10:20-12;40");
+		System.out.format("Date: %15sTime: %8s", "Monday", "12:40-1:55");
+		System.out.format("Date: %15sTime: %8s", "Monday", "2:00-3:15");
+		
+		/*
+		 * This is a block comment.
+		 * You can add notes on multiple lines
+		 */
+	}
+
+
+}
