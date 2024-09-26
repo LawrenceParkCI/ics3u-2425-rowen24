bde833b6584735ddc578260d16364be986a1a76e
343119186 <343119186@C-MXL2393F1F.tdsb.on.ca>
2024-09-26 11:41:50 -0400

    This is some information about me

--- ICS3U-RoweN/src/unit1/AboutMe.java
+++ ICS3U-RoweN/src/unit1/AboutMe.java
@@ -2,7 +2,7 @@ package unit1;
 /**
  * Description: This program prints Hello World, and
  * a second statement.
- * Date: Sept 19, 2024
+ * Date: September 19, 2024
  * @author Noah Rowe
  */
 public class AboutMe{
@@ -13,14 +13,13 @@ public class AboutMe{
 		System.out.println("My teacher is Ms.Kemp");
 		System.out.println("In grade 11");
 		System.out.println("Go Panthers!");
-		System.out.println("--------------------------------------------");
+		System.out.println("______________________________________________________________________");
 		System.out.format("\n");
-		System.out.println("______________________________________________");
-		System.out.format(" Date: \t \t   Time: \t \t   Period 1 \t \t   period 2 \t \t   period 3 \t \t   Period 4 \t \t");
-		System.out.format("Date: %15sTime: %8s", "Monday", "9:00-10:20");
-		System.out.format("Date: %15sTime: %8s", "Monday", "10:20-12;40");
-		System.out.format("Date: %15sTime: %8s", "Monday", "12:40-1:55");
-		System.out.format("Date: %15sTime: %8s", "Monday", "2:00-3:15");
+		System.out.println("======================================================================");
+		System.out.println(" Date: \t \t   Time: \t \t   Period 1 \t \t   period 2 \t \t   period 3 \t \t   Period 4 \t \t");
+		System.out.println(""
+		System.out.println(""
+		System.out.println(""
 		
 		/*
 		 * This is a block comment.
---
+++ ICS3U-RoweN/src/unit1/Bingo.java
@@ -0,0 +1,21 @@
+package unit1;
+
+public class Bingo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("1. The caller randomly pulls a numbered bingo ball.");
+System.out.println("2. The number is placed on the bingo board and called out.");
+System.out.println("3. Players look for the called number on their bingo card.");
+System.out.println("4. If the number is located, it is marked off");
+System.out.println("5. Steps 1 to 4 are repeated until a player matxhes the BINGO");
+System.out.println("6. The winning player yells BINGO!");
+System.out.println("")
+System.out.println("")
+System.out.println("")
+System.out.println("")
+System.out.println("")
+System.out.println("")
+	}
+
+}
